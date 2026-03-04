e9893129545f8e42cfa713c6f65a3f3fb6a75764
suri-codes <suri312006@gmail.com>
2026-03-03 22:08:41 -0800

    feat: init flake

--- README.md
+++ README.md
@@ -4,4 +4,11 @@ My personal-knowledge-system, with deeply integrated
 task tracking and long term goal planning capabilities.
 
 This is a continuation of my previous project, [TARS](https://github.com/suri-codes/TARS).
-l
+
+## Name Origin
+
+Derived from [galaxy filaments](https://en.wikipedia.org/wiki/Galaxy_filament),
+with clusters being notes, and filaments being the links between them.
+
+
+
---
+++ flake.nix
@@ -0,0 +1,91 @@
+{
+  description = "An empty flake template that you can adapt to your own environment";
+
+  # Flake inputs
+  inputs = {
+
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # unstable Nixpkgs
+
+    fenix = {
+      url = "https://flakehub.com/f/nix-community/fenix/0.1";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  # Flake outputs
+  outputs =
+    { self, ... }@inputs:
+
+    let
+      # The systems supported for this flake
+      supportedSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [
+                inputs.self.overlays.default
+              ];
+            };
+          }
+        );
+    in
+    {
+
+      overlays.default = final: prev: {
+        rustToolchain =
+          with inputs.fenix.packages.${prev.stdenv.hostPlatform.system};
+          combine (
+            with stable;
+            [
+              clippy
+              rustc
+              cargo
+              rustfmt
+              rust-src
+            ]
+          );
+      };
+
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShellNoCC {
+            # The Nix packages provided in the environment
+            # Add any you need here
+            packages = with pkgs; [
+
+              rustToolchain
+              openssl
+              pkg-config
+              cargo-deny
+              cargo-edit
+              cargo-watch
+              rust-analyzer
+
+              typst
+              typstyle
+            ];
+
+            # Set any environment variables for your dev shell
+            env = {
+              RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+            };
+
+            # Add any shell logic you want executed any time the environment is activated
+            shellHook = "";
+          };
+        }
+      );
+    };
+}
